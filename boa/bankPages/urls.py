2340fb5dc58afe1815bbbac5f780f0c14ca6847c
unknown <elotgodsplan2@gmail.com>
2025-03-12 00:24:47 +0100

    Update bankPages/urls.py

--- boa/bankPages/urls.py
+++ boa/bankPages/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
      path('', views.home, name='home'),  # Public Home Page
+     path('dashboard/', views.dashboard, name='dashboard'),
+     path('deposit/', views.deposit, name='deposit'),
+     path('withdraw/', views.withdraw, name='withdraw'),
+     path('transactions/', views.transactions, name='transactions'),
+     path('profile/', views.profile, name='profile'),
 ]
