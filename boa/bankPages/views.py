bc8a0edb4af225611958a6ceee14494ee0e2b782
unknown <elotgodsplan2@gmail.com>
2025-03-12 00:18:30 +0100

    Update bankpages app views.py

--- boa/bankPages/views.py
+++ boa/bankPages/views.py
@@ -5,22 +5,23 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request):
     return render(request, 'bankPages/home.html')
+
 @login_required
 def dashboard(request):
-    return HttpResponse("This is the secured dashboard.")
+    return render(request, 'bankPages/dashboard.html')
 
 @login_required
 def deposit(request):
-    return HttpResponse("Deposit page.")
+    return render(request, 'bankPages/deposit.html')
 
 @login_required
 def withdraw(request):
-    return HttpResponse("Withdrawal page.")
+    return render(request, 'bankPages/withdraw.html')
 
 @login_required
 def transactions(request):
-    return HttpResponse("Transactions history page.")
+    return render(request, 'bankPages/transactions.html')
 
 @login_required
 def profile(request):
-    return HttpResponse("Profile page.")
+    return render(request, 'bankPages/profile.html')
