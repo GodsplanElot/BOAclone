17a47f5a60b1ab96e6c5cf71545d32a0ea8bb3dc
unknown <elotgodsplan2@gmail.com>
2025-03-14 00:11:43 +0100

    Create views for message app

--- boa/messaging/views.py
+++ boa/messaging/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from .models import Message
 # Create your views here.
+
+@login_required
+def user_messages(request):
+    messages = Message.objects.filter(recipient=request.user).order_by('-timestamp')
+    return render(request, 'messaging/messages.html', {'messages': messages})
