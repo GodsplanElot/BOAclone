3bb28648fd80413ae2e74b5eaf6adca0aa3d1fa5
unknown <elotgodsplan2@gmail.com>
2025-03-13 23:18:30 +0100

    Modify message Model, register in the admin

--- boa/messaging/admin.py
+++ boa/messaging/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Message
 
 # Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'subject', 'timestamp', 'is_read')
+    list_filter = ('is_read', 'timestamp')
+    search_fields = ('sender__username', 'receiver__username', 'subject', 'content')
+
+admin.site.register(Message, MessageAdmin)
+
--- boa/messaging/models.py
+++ boa/messaging/models.py
@@ -5,8 +5,9 @@ from django.utils.timezone import now
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_messages")
-    message = models.TextField()
-    timestamp = models.DateTimeField(default=now)
+    subject = models.CharField(max_length=255)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
 
     def __str__(self):
