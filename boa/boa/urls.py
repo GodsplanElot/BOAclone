03f4d897130680a0612e5889e84e127a4f206b86
unknown <elotgodsplan2@gmail.com>
2025-03-14 00:09:30 +0100

    Add message app urls.py to the main urls.py

--- boa/boa/urls.py
+++ boa/boa/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bankPages.urls')),
+    path('messaging/', include('messaging.urls')),
     path('auth/', include('authSystem.urls')),
 ]
