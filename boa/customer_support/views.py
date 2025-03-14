e74f395dabf4a1fb0fddcab9b9a6aca9f092f150
unknown <elotgodsplan2@gmail.com>
2025-03-14 01:52:25 +0100

    Update Customer support views.py

--- boa/customer_support/views.py
+++ boa/customer_support/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from .models import SupportMessage
 from .forms import SupportMessageForm
 
-# Create your views here.
-
 @login_required
 def support_messages(request):
-    messages = SupportMessage.objects.filter(user=request.user)
+    if request.user.is_staff:  
+        # Admins see all messages
+        messages = SupportMessage.objects.all()
+    else:
+        # Users see only their own messages
+        messages = SupportMessage.objects.filter(user=request.user)
     return render(request, 'customer_support/messages.html', {'messages': messages})
 
 @login_required
@@ -16,7 +20,7 @@ def send_support_message(request):
         form = SupportMessageForm(request.POST)
         if form.is_valid():
             message = form.save(commit=False)
-            message.user = request.user
+            message.user = request.user  # Set the logged-in user as sender
             message.save()
             return redirect('support_messages')
     else:
