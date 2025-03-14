58090677e5104e97e022c27c48851bda6899e601
unknown <elotgodsplan2@gmail.com>
2025-03-14 01:14:28 +0100

    Register model in the admin site

--- boa/customer_support/admin.py
+++ boa/customer_support/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import SupportMessage
 # Register your models here.
+
+@admin.register(SupportMessage)
+class SupportMessageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'subject', 'is_resolved', 'timestamp')
+    search_fields = ('user__username', 'subject', 'message')
+    list_filter = ('is_resolved',)
+
+admin.site.site_header = "Customer Support Admin"
