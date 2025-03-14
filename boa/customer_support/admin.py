24b4a08d7d4a39c00c601999e33aaf90b65919cd
unknown <elotgodsplan2@gmail.com>
2025-03-14 02:05:30 +0100

    Update Cutomer_support admin.py file

--- boa/customer_support/admin.py
+++ boa/customer_support/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 from .models import SupportMessage
-# Register your models here.
 
 @admin.register(SupportMessage)
 class SupportMessageAdmin(admin.ModelAdmin):
     list_display = ('user', 'subject', 'is_resolved', 'timestamp')
     search_fields = ('user__username', 'subject', 'message')
     list_filter = ('is_resolved',)
+    readonly_fields = ('user', 'subject', 'message', 'timestamp')  # Users cannot edit these
 
-admin.site.site_header = "Customer Support Admin"
+    def get_queryset(self, request):
+        """Restrict normal users from seeing messages in admin."""
+        qs = super().get_queryset(request)
+        if request.user.is_staff:
+            return qs  # Staff can see all messages
+        return qs.filter(user=request.user)  # Users can only see their own
+
+    def has_change_permission(self, request, obj=None):
+        """Allow only staff users to respond to messages."""
+        if obj and not request.user.is_staff:
+            return False
+        return super().has_change_permission(request, obj)
