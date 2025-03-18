3681cca67de0c69621361b934ecc2ffb9015c662
unknown <elotgodsplan2@gmail.com>
2025-03-18 18:23:14 +0100

    Update userdetails views.py

--- boa/userdetails/views.py
+++ boa/userdetails/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile
+from .forms import UserProfileForm
 # Create your views here.
+
+
+
+
+@login_required
+def update_profile(request):
+    profile, created = UserProfile.objects.get_or_create(user=request.user)
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserProfileForm(instance=profile)
+
+    return render(request, 'userdetails/update_profile.html', {'form': form})
