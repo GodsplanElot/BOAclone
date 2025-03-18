d9d234c9fd50d4f1b28cbed126eea65c8d163b50
unknown <elotgodsplan2@gmail.com>
2025-03-18 18:03:50 +0100

    Register model in Admin Panel

--- boa/userdetails/admin.py
+++ boa/userdetails/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import UserDetails
 # Register your models here.
+
+
+@admin.register(UserDetails)
+class UserDetailsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'account_type', 'branch', 'kyc_status', 'date_joined')
+    list_filter = ('kyc_status', 'account_type')
