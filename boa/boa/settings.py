2b79d018bb9553628513e6c4844b1eaff0f2e2e2
unknown <elotgodsplan2@gmail.com>
2025-03-18 18:01:37 +0100

    Modify model of userdetail app and registered the app in the settings.py

--- boa/boa/settings.py
+++ boa/boa/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'messaging',
     'customer_support',
     'withdrawals',
+    'userdetails',
 ]
 
 MIDDLEWARE = [
--- boa/userdetails/models.py
+++ boa/userdetails/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class UserDetails(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
+    # Basic Info
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField()
+
+
+
+    # Account Info
+    ACCOUNT_TYPES = [('Basic', 'Basic'), ('Premium', 'Premium')]
+    account_type = models.CharField(max_length=20, choices=ACCOUNT_TYPES, default='Basic')
+    branch = models.CharField(max_length=100)
+    kyc_status = models.BooleanField(default=False)
+    linked_account = models.CharField(max_length=100, blank=True, null=True)
+    date_joined = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
