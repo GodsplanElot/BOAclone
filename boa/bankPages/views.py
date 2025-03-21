8212154be6f71e0cd87ffb34400287ae8f1ec5b9
unknown <elotgodsplan2@gmail.com>
2025-03-21 23:52:23 +0100

    Modify the profile page

--- boa/bankPages/views.py
+++ boa/bankPages/views.py
@@ -48,4 +48,6 @@ def transactions(request):
 @login_required
 def profile(request):
     profile, created = UserDetails.objects.get_or_create(user=request.user)
+    user_profile, created = UserProfile.objects.get_or_create(user=request.user)
+
     return render(request, 'bankPages/profile.html', {'profile': profile})
