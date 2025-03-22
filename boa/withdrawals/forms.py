754f7d2cc05e1ac8459333cebaf75ce261522b31
unknown <elotgodsplan2@gmail.com>
2025-03-22 06:43:50 +0100

    Update forms.py file

--- boa/withdrawals/forms.py
+++ boa/withdrawals/forms.py
@@ -4,4 +4,16 @@ from .models import WithdrawalRequest
 class WithdrawalForm(forms.ModelForm):
     class Meta:
         model = WithdrawalRequest
-        fields = ['amount', 'payment_method']
+        fields = ['account_number', 'account_holder_name', 'sort_code', 'amount', 'payment_method']
+
+        widgets = {
+            'account_number': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Account Number'}),
+            'account_holder_name': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Account Holder Name'}),
+            'sort_code': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Sort Code'}),
+            'amount': forms.NumberInput(attrs={'class': 'form-input', 'placeholder': 'Amount'}),
+            'payment_method': forms.Select(attrs={'class': 'form-input'}, choices=[
+                ('Bank Transfer', 'Bank Transfer'),
+                ('PayPal', 'PayPal'),
+                ('Crypto Wallet', 'Crypto Wallet'),
+            ]),
+        }
