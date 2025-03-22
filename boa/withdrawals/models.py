0ed91940ec0d9955d9e86e6edbe37c969de93a0a
unknown <elotgodsplan2@gmail.com>
2025-03-22 06:41:30 +0100

    Modify models of the withdrawal request

--- boa/withdrawals/models.py
+++ boa/withdrawals/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
-
 
 class WithdrawalRequest(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_number = models.CharField(max_length=50, null=True, blank=True)
+    account_holder_name = models.CharField(max_length=100, null=True, blank=True)
+    sort_code = models.CharField(max_length=20, null=True, blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     payment_method = models.CharField(max_length=100)
     status = models.CharField(
